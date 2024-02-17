cf6bf45be5a92003ec537deaa50696a772d7735d
DimaTivator <andriianov164@gmail.com>
2024-02-18 00:22:41 +0300

    FIX: fix criteria functions. Now they return float

--- compmath/_criterion.py
+++ compmath/_criterion.py
@@ -1,15 +1,15 @@
 from compmath.linalg import Matrix
 
 
-def abs_deviation(prev: list, cur: list, eps=1e-5) -> bool:
-    return max([abs(prev[i] - cur[i]) for i in range(len(prev))]) < eps
+def abs_deviation(prev: list, cur: list):
+    return max([abs(prev[i] - cur[i]) for i in range(len(prev))])
 
 
-def relative_diff(prev: list, cur: list, eps=1e-5) -> bool:
-    return max([abs((prev[i] - cur[i]) / cur[i]) for i in range(len(prev))]) < eps
+def relative_diff(prev: list, cur: list):
+    return max([abs((prev[i] - cur[i]) / cur[i]) for i in range(len(prev))])
 
 
-def discrepancy_diff(A: Matrix, b: Matrix, x: Matrix, eps=1e-5) -> bool:
+def discrepancy_diff(A: Matrix, b: Matrix, x: Matrix):
     r = A * x - b
-    return max([r[i] for i in range(len(r))]) < eps
+    return max([r[i] for i in range(len(r))])
 
